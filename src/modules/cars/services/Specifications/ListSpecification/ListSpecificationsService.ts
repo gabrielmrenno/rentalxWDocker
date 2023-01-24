aeb7fb83dc78c8d95d6fcea6c574a62ca11b6e86
gabrielrennoprime <grenno@ext.grupo-artico.com>
2023-01-24 15:03:34 -0300

    [FIX] add tsconfig-path and setting scripts on packge.json

--- src/modules/cars/services/Specifications/ListSpecification/ListSpecificationsService.ts
+++ src/modules/cars/services/Specifications/ListSpecification/ListSpecificationsService.ts
@@ -1,13 +1,12 @@
 import { inject, injectable } from "tsyringe";
 
 import { Specification } from "../../../entities/specification";
-import { SpecificationsRepository } from "../../../repositories/implementations/SpecificationsRepository";
 import { ISpecificationsRepository } from "../../../repositories/ISpecificationsRepository";
 
 @injectable()
 export class ListSpecificationsService {
     constructor(
-        @inject(SpecificationsRepository)
+        @inject("SpecificationsRepository")
         private specificationRepository: ISpecificationsRepository
     ) {}
     async execute(): Promise<Specification[]> {
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
+import "reflect-metadata";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import "reflect-metadata";
-
 import { connectDatabase } from "./database/ormconfig";
-import "./shared/container";
+import "@shared/container";
 import { AppError } from "./errors/AppError";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
